d6666769d49a02b9f3f6cd739962e124f696335b
rarz1 <raulroys1@gmail.com>
2026-07-23 12:33:58 -0500

    refactor: store banner as full-width top header bar

--- client/src/components/StoreBanner.tsx
+++ client/src/components/StoreBanner.tsx
@@ -7,12 +7,14 @@ interface Props {
 export default function StoreBanner({ settings }: Props) {
   return (
     <div className="store-banner">
-      <div className="store-banner__glass">
+      <div className="store-banner__inner">
         {settings.logo_url && (
           <img src={settings.logo_url} alt="" className="store-banner__logo" />
         )}
-        <h1 className="store-banner__title">{settings.store_title}</h1>
-        <p className="store-banner__subtitle">{settings.store_subtitle}</p>
+        <div className="store-banner__text">
+          <span className="store-banner__title">{settings.store_title}</span>
+          <span className="store-banner__subtitle">{settings.store_subtitle}</span>
+        </div>
       </div>
     </div>
   );
